260e0e9177d2a3b3edba7025d22fdd345542da01
arthur <arthurdoespiritosanto1@gmail.com>
2026-03-24 13:02:45 -0300

    Primeiros estudos até construtores

---
+++ src/entidade/Usuario.java
@@ -0,0 +1,20 @@
+package entidade;
+
+public class Usuario {
+
+    public String email;
+    public String senha;
+
+    public Usuario(String email, String senha){
+
+        System.out.println("AUTENTIAÇÃO POR EMAIL E SENHA");
+
+
+    }
+
+    public Usuario(String telefone){
+
+        System.out.println("AUTENTIAÇÃO POR TELEFONE");
+    }
+
+}
---
+++ src/program/Main.java
@@ -0,0 +1,17 @@
+package program;
+
+import entidade.Usuario;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Usuario usuario = new Usuario("999999999", "DFSF");
+
+
+    }
+    }
